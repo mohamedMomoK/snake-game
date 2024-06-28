e38673e1f1a8258f84fddb3bc7b8055a498ea7f6
mohamed <143962984+mohamedMomoK@users.noreply.github.com>
2024-06-28 11:59:52 -0400

    Initial project setup

---
+++ SnakeGame/src/com/mohamedMomok/snakegame/SnakeGame.java
@@ -0,0 +1,14 @@
+package com.mohamedMomok.snakegame;
+
+import javax.swing.*;
+
+public class SnakeGame {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Snake Game");
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800,600);
+		frame.setVisible(true);
+	}
+
+}
